7270013ea7b458ff3b0bd81bdd0787c7a7170729
LINUX <linux@localhost.localdomain>
2023-05-24 18:59:43 +0300

    Multi User Login

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'tulusdeveloper@gmail.com',
+                'password' => Hash::make('Admin@12345'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+             // Agent
+             [
+                'name' => 'Agent',
+                'username' => 'agent',
+                'email' => 'agent@gmail.com',
+                'password' => Hash::make('Agent@12345'),
+                'role' => 'agent',
+                'status' => 'active',
+             ],
+              // User
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('User@12345'),
+                'role' => 'user',
+                'status' => 'active',
+            ],
+        ]);
+    }
+}
