f050b16ce78fb75b59d41a53dcb9aa5882b0ba76
LINUX <linux@localhost.localdomain>
2023-05-24 22:51:52 +0300

    Middleware-Role-Implementatopn

--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,14 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+Route::middleware(['auth','role:admin'])->group(function(){
+
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+}); // End Group Admin Middleware
+
+Route::middleware(['auth','role:agent'])->group(function(){
+
+    Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+
+}); // End Group Agent Middleware
