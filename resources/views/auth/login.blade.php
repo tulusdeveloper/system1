c0c6f9aaaed2e11a87d865ce39f18c09e3841359
LINUX <linux@localhost.localdomain>
2023-05-27 20:04:01 +0300

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,142 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="NobleUI">
+	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="login" :value="__('Email/Name/Phone')" />
-            <x-text-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
-        </div>
+	<title>Admin Login</title>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <style type="text/css">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        .authlogin-side-wrapper{
+            width: 100%;
+            height: 100%;
+            background-image: url("{{ asset('upload/login.png') }}");
+        }
+    </style>
+    <!-- @if (isset($reload) && $reload)
+      <script>
+          window.onload = function() {
+              location.reload();
+          };
+      </script>
+    @endif -->
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+  <!-- End fonts -->
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+	<!-- core:css -->
+	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/core/core.css') }}">
+	<!-- endinject -->
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+	<!-- Plugin css for this page -->
+	<!-- End plugin css for this page -->
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+	<!-- inject:css -->
+	<link rel="stylesheet" href="{{ asset('backend/assets/fonts/feather-font/css/iconfont.css') }}">
+	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+	<!-- endinject -->
+
+  <!-- Layout styles -->  
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/demo2/style.css') }}">
+  <!-- End layout styles -->
+
+  <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
+</head>
+<body>
+	<div class="main-wrapper">
+		<div class="page-wrapper full-page">
+			<div class="page-content d-flex align-items-center justify-content-center">
+
+				<div class="row w-100 mx-0 auth-page">
+					<div class="col-md-8 col-xl-6 mx-auto">
+						<div class="card">
+							<div class="row">
+                <div class="col-md-4 pe-md-0">
+                  <div class="authlogin-side-wrapper">
+
+                  </div>
+                </div>
+                
+                <div class="col-md-8 ps-md-0">
+                  <div class="auth-form-wrapper px-4 py-5">
+                    <a href="#" class="noble-ui-logo logo-light d-block mb-2">E-<span>SYSTEM</span></a>
+                    <h5 class="text-muted fw-normal mb-4">Welcome back! Log in to your account.</h5>
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <!-- Email Address -->
+                        <div class="mb-3">
+                            <x-input-label for="login" :value="__('Email/Name/Phone')" />
+                            <x-text-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
+                        </div>
+
+                        <!-- Password -->
+                        <div class="mb-3">
+                            <x-input-label for="password" :value="__('Password')" />
+
+                            <x-text-input id="password" class="block mt-1 w-full"
+                                            type="password"
+                                            name="password"
+                                            required autocomplete="current-password" />
+
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+
+                        <!-- Remember Me -->
+                        <div class="block mb-3">
+                            <label for="remember_me" class="inline-flex items-center">
+                                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                            </label>
+                        </div>
+
+                        <div class="flex items-center justify-end mb-3">
+                            @if (Route::has('password.request'))
+                                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                            @endif
+
+                            <x-primary-button class="ml-3">
+                                {{ __('Log in') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+              </div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- core:js -->
+	<script src="{{ asset('backend/assets/vendors/core/core.js') }}"></script>
+	<!-- endinject -->
+
+	<!-- Plugin js for this page -->
+	<!-- End plugin js for this page -->
+
+	<!-- inject:js -->
+	<script src="{{ asset('backend/assets/vendors/feather-icons/feather.min.js') }}"></script>
+	<script src="{{ asset('backend/assets/js/template.js') }}"></script>
+	<!-- endinject -->
+
+	<!-- Custom js for this page -->
+	<!-- End custom js for this page -->
+
+</body>
+</html>
