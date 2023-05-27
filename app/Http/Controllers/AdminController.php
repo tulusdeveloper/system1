6e4461d88c4382f82c3d1b6116d99f14499ddb69
LINUX <linux@localhost.localdomain>
2023-05-27 04:58:22 +0300

    Introduction of Admin Profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 // use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
+        if (Auth::guard('web')->check()) {
         return view('admin.index');
+        }
+        return redirect('/admin/login');
 
     } // End Method
 
@@ -21,14 +25,16 @@ class AdminController extends Controller
 
         $request->session()->regenerateToken();
 
-
-
         return redirect('/admin/login');
-
     } // End Method
 
     public function AdminLogin(){
-
         return view('admin.admin_login');
+    }// End Method
+
+    public function AdminProfile(){
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
     }// End Method
 }
